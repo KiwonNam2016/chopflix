f41a6cf3ab8c939f5b418af4dcef7b8ef22b3f39
eddiezhaor <eddiezhaor@gmail.com>
2018-01-20 16:05:30 -0600

    animation

--- assets/animation.js
+++ assets/animation.js
@@ -7,12 +7,26 @@ function animation(){
     var watching=$(".whatchaWatching");
     var tl1=new TimelineLite();
     var tl3=new TimelineLite();
+    var tl6=new TimelineLite();
+    var tl7=new TimelineLite();
+    tl6
+    .from($("#khamel"), 2,{rotationX:-3600,transformOrigin:'50% 50%', ease:Power2.easeInOut})
+    .from($("#rachel"), 2,{rotationX:-3600,transformOrigin:'50% 50%', ease:Power2.easeInOut},'-=2')
+    .from($("#amy"), 2,{rotationX:-3600,transformOrigin:'50% 50%', ease:Power1.easeOut},'-=2')
+    .from($("#eddie"), 2,{rotationX:-3600,transformOrigin:'50% 50%', ease:Elastic.easeOut},'-=2')
+    .to($("#eddie"), 2,{y:600, autoAlpha:0, transformOrigin:'50% 50%', ease:Elastic.easeOut},'-=0.5')
+    .to($("#amy"), 2,{y:600, autoAlpha:0, transformOrigin:'50% 50%', ease:Elastic.easeOut},'-=1.5')
+    .to($("#eddie"),1,{y:0, autoAlpha:10, transformOrigin:'50% 50%', ease:Elastic.easeOut},'-=0.5')
+    .to($("#amy"), 1,{y:0, autoAlpha:10, transformOrigin:'50% 50%', ease:Elastic.easeOut},'-=0.4')
+    
+
     tl3.from(something,2,{opacity:100,y:50, onUpdate:onUpdate, onComplete:onComplete});
     // var tl=new TimelinLite();
     tl1.to(icon,1,{rotationX:-360,transformOrigin:'50% 50%', ease:Power2.easeInOut})
     
     tl3.pause();
     tl1.pause();
+    tl6.pause();
     TweenLite.from(inputBox,1,{opacity:0,y:50});
   
     
@@ -42,7 +56,12 @@ function animation(){
        tl1.restart();
        
     })
+
     
+    $(".team-member").on("click",function(){
+        tl6.play();
+        tl6.restart();
+    })
 }
 
 
